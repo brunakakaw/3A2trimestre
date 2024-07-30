dc79f397a2151dead8cd6a4bfab816a4b8a9c0c8
kakaw <107406703+brunakakaw@users.noreply.github.com>
2024-07-30 10:41:26 -0300

    .

--- script.js
+++ script.js
@@ -59,4 +59,18 @@ function mostraAlternativas() {
 }
 
 mostraPergunta();
-COPIAR CÓDIGO
+
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia"
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "afirmacao"
+            }
+        ]
+},
